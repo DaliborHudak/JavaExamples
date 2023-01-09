971b2f7f93a7d3e27b89b2c8c7f97951a2dac008
DaliborHudak <daliborhudak123456798@gmail.com>
2023-01-09 21:05:37 +0100

    created gradetest

---
+++ src/GradeTest.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*
+Vytvorte prostrednictvom vstupu z konzoly rozhodovací test, ktorý vypíše známku A, B alebo C podľa týchto kritérií:
+    Ak je percento nad 90, tak vypíše známku A
+    Ak je percento nad 75, tak vypíše známku B
+    Ak je percento nad 65, tak vypíše známku C
+    Použite metódu pre rozhodovanie.
+*/
+
+public class GradeTest {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Zadaj percento testu:");
+        int grade = scn.nextInt(); // input value for grade is int
+
+        if (grade > 90) {
+            System.out.println("Dostal si známku A");
+        } else if (grade > 75) {
+            System.out.println("Dostal si známku B");
+        } else if (grade > 65) {
+            System.out.println("Dostal si známku C");
+        } else {
+            System.out.println("Dostal si známku F Nespravil si test.");
+        }
+    }
+}
